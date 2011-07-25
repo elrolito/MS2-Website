4f42a40554386aadac249d3c01eccc5b00ff226b
Rolando Henry <elrolito@me.com>
2011-07-24 20:33:07 -0400

    adding contact form and email submodule

---
+++ application/classes/controller/contact.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Contact extends Controller_Layout {
+	
+	public function action_index()
+	{
+		
+	}
+}
---
+++ application/classes/view/contact.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class View_Contact extends View_Layout {
+	
+	public $title = 'Contact us for a quote!';
+	
+	
+}
